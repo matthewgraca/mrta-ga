08f8cda54f3342726ba26cca5953e511d2cad66e
Matthew Graca <matthew.graca@gmail.com>
2024-11-20 20:27:24 -0800

    error checking for constructor

--- src/genetic_algorithm.py
+++ src/genetic_algorithm.py
@@ -1,12 +1,11 @@
 class GeneticAlgorithm:
     '''
-        Parameters:
-            pop_size: Population size, default 100
-            crossover: Crossover method, default partially mapped crossover 
-                (pmx)
-            mutation: Mutation method, default inverse mutation
-            pc: Probability of crossover, default 0.4
-            pm: Probability of mutation, default 0.6
+    Parameters:
+        pop_size: Population size, default 100
+        crossover: Crossover method, default partially mapped crossover (pmx)
+        mutation: Mutation method, default inverse mutation
+        pc: Probability of crossover, default 0.4
+        pm: Probability of mutation, default 0.6
     '''
     def __init__(
             self, 
@@ -16,15 +15,42 @@ class GeneticAlgorithm:
             pc=0.4, 
             pm=0.6
     ):
+        # check parameters
+        self.__validate_params(pop_size, crossover, mutation, pc, pm)
+
         self.pop_size = pop_size
-        self.crossover = crossover
+        self.crossover = crossover 
         self.mutation = mutation
         self.pc = pc
         self.pm = pm
 
     '''
-        Returns: The current parameter setup of the genetic algorithm as 
-        a dictionary.
+    Validates the parameters. Throws errors if they are not valid
+    '''
+    def __validate_params(self, pop_size, crossover, mutation, pc, pm):
+        # dictionary of valid parameters
+        valid_params = {
+            'crossover' : {'pmx'},
+            'mutation' : {'inverse'}
+        }
+
+        # limit pop size to [1, 100000]
+        if not 1 <= pop_size <= 100000:
+            raise ValueError(f"pop_size should be in the range [1, 100000]")
+        # ensure crossover and mutation methods are defined
+        if crossover not in valid_params['crossover']: 
+            raise ValueError(f"Crossover method \'{crossover}\' is not defined")
+        if mutation not in valid_params['mutation']:
+            raise ValueError(f"Mutation method \'{mutation}\' is not defined")
+        # limit pc to [0.0, 1.0]
+        if not 0.0 <= pc <= 1.0:
+            raise ValueError("pc should be in the range [0.0, 1.0]")
+        if not 0.0 <= pm <= 1.0:
+            raise ValueError("pm should be in the range [0.0, 1.0]")
+
+    '''
+    Returns: The current parameter setup of the genetic algorithm as a 
+        dictionary.
     '''
     def get_parameters(self):
         return {
@@ -34,6 +60,3 @@ class GeneticAlgorithm:
             'pc' : self.pc,
             'pm' : self.pm
         }
-
-    def test(self):
-        return 0
--- test/genetic_algorithm_test.py
+++ test/genetic_algorithm_test.py
@@ -3,7 +3,7 @@ from src.genetic_algorithm import GeneticAlgorithm
 
 class GeneticAlgorithmTest(unittest.TestCase):
     '''
-        Test initialization, parameters
+    Test initialization, parameters
     '''
     def test_init(self):
         ga = GeneticAlgorithm()
@@ -16,4 +16,24 @@ class GeneticAlgorithmTest(unittest.TestCase):
             'pm' : 0.6
         }
         self.assertEqual(actual, expected)
+   
+    def test_valid_parameters(self):
+        ga = GeneticAlgorithm(pop_size=1000, pc=0.1, pm=0.3)
+        actual = ga.get_parameters()
+        expected = {
+            'pop_size' : 1000,
+            'crossover' : 'pmx',
+            'mutation' : 'inverse',
+            'pc' : 0.1,
+            'pm' : 0.3
+        }
+        self.assertEqual(actual, expected)
 
+    def test_invalid_pop_size_parameter(self):
+        self.assertRaises(ValueError, GeneticAlgorithm, pop_size=0)
+
+    def test_invalid_pc_parameter(self):
+        self.assertRaises(ValueError, GeneticAlgorithm, pc=-1.0)
+
+    def test_invalid_mutation_parameter(self):
+        self.assertRaises(ValueError, GeneticAlgorithm, mutation='buh')
